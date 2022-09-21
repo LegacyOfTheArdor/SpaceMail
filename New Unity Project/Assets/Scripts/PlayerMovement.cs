2dfeaf9d0965cacfd189b4e48bf66d2a222bb04d
LegacyOfTheArdor <95230814+LegacyOfTheArdor@users.noreply.github.com>
2022-09-21 12:16:40 +0200

    Player camera and added movement

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public CharacterController playercontroller;
+
+	public float momentum = 6f;
 
     // Update is called once per frame
     void Update()
