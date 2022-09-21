bb6c8143b88bb955044bb19fdee268fff048333d
LegacyOfTheArdor <95230814+LegacyOfTheArdor@users.noreply.github.com>
2022-09-21 13:30:02 +0200

    working on movement

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		/*float forward = Input.GetAxisRaw("forward");
+		Vector3 direction = new Vector3(0f, 0f, forward).normalized;
+
+			//if (direction.magnitude >= 0.1f)
+			if (Input.GetKeyDown(KeyCode.W))
+			{
+				playercontroller.Move(direction * momentum * Time.deltaTime);
+			}*/
     }
 }
