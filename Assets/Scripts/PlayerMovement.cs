805127b10bc762e4626ebbe14fe56f3b3541a6b0
LegacyOfTheArdor <95230814+LegacyOfTheArdor@users.noreply.github.com>
2022-09-22 14:15:12 +0200

    spaceship forward movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	public CharacterController playercontroller;
+	public Transform ThirdPersonCamera;
 
-	public float momentum = 6f;
+	public float momentum = 30f;
 
     // Update is called once per frame
     void Update()
     {
-		/*float forward = Input.GetAxisRaw("forward");
+		float forward = Input.GetAxisRaw("Vertical");
 		Vector3 direction = new Vector3(0f, 0f, forward).normalized;
 
-			//if (direction.magnitude >= 0.1f)
-			if (Input.GetKeyDown(KeyCode.W))
+		if (Input.GetKey(KeyCode.W))
+		{
+			if (direction.magnitude >= 0.1f)
 			{
 				playercontroller.Move(direction * momentum * Time.deltaTime);
-			}*/
+			}
+		}
     }
 }
