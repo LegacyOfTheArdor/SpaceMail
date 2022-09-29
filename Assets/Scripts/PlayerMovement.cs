aac85a7599bb508a20328e11a4d5f7ed65bc3e89
LegacyOfTheArdor <95230814+LegacyOfTheArdor@users.noreply.github.com>
2022-09-29 11:24:11 +0200

    flying spaceship

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,62 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public CharacterController playercontroller;
-	public Transform ThirdPersonCamera;
-
+	
+	
+	/*public Transform playercamera;
+	public float turnSpaceship = 0.1f;
+	float turnvelocity;*/
+	
 	public float momentum = 30f;
+	private float activeMomentum;
 
-    // Update is called once per frame
-    void Update()
+	public float TurnSpeed = 90f;
+	private Vector2 SteerInput, screenCenter, mouseDistance;
+
+	private void Start()
+	{
+		screenCenter.x = Screen.width * 0.5f;
+		screenCenter.y = Screen.height * 0.5f;
+	}
+	// Update is called once per frame
+	void Update()
     {
-		float forward = Input.GetAxisRaw("Vertical");
-		Vector3 direction = new Vector3(0f, 0f, forward).normalized;
+		steer();
+		Accelerate();
 
-		if (Input.GetKey(KeyCode.W))
-		{
-			if (direction.magnitude >= 0.1f)
-			{
-				playercontroller.Move(direction * momentum * Time.deltaTime);
-			}
-		}
     }
+
+	public int activespeed()
+	{
+		if (Input.GetKey("w"))
+		{
+			return 1;
+		}
+		return 0;
+	}
+
+	private void steer()
+	{
+		SteerInput.x = Input.mousePosition.x;
+		SteerInput.y = Input.mousePosition.y;
+
+		mouseDistance.x = (SteerInput.x - screenCenter.x) / screenCenter.y;
+		mouseDistance.y = (SteerInput.y - screenCenter.y) / screenCenter.y;
+
+		mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+
+		transform.Rotate(-mouseDistance.y * TurnSpeed * Time.deltaTime, mouseDistance.x * TurnSpeed * Time.deltaTime, 0f, Space.Self);
+	}
+
+	private void Accelerate()
+	{
+		activeMomentum = activespeed() * momentum;
+
+		transform.position += transform.forward * activeMomentum * Time.deltaTime;
+	}
+
+	private void centerMouse()
+	{
+
+	}
 }
